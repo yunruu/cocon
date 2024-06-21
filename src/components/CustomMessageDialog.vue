c30c420a945d897f4f5371c21c290ae31bf25ae2
Yun Ru Chin <go25dox@mytum.de>
2024-06-21 03:04:19 +0300

    feat: create custom message dialog

---
+++ src/components/CustomMessageDialog.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="fixed top-6 left-0 w-full flex justify-center">
+        <div v-if="props.show" class="flex gap-4 bg-teal-100 p-3 w-5/6 rounded-md border border-teal-400 items-center">
+            <img src="../assets/icons/success.svg" alt="Success icon" class="w-6 h-6" />
+            <h2 class="text-md font-semibold">{{ message }}</h2>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['message', 'show']);
+</script>
