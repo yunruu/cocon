4d63891e44505ae1d1d5bc5f08ff81e72d8218a4
Yun Ru Chin <go25dox@mytum.de>
2024-06-21 03:04:33 +0300

    feat: set up form view

---
+++ src/views/FormView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="px-4 py-6 bg-indigo-500">
+        <h1 class="text-2xl font-bold text-indigo-900 mt-4 mb-1 uppercase">Cocon</h1>
+    </div>
+    <CurrencyForm />
+</template>
+
+<script setup>
+import CurrencyForm from '../components/CurrencyForm.vue'
+</script>
